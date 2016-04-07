fcf7db7635b0aef67493edbffdc460eb62ea46b3
Brandon Hoffman <brandon.michael.hoffman@gmail.com>
2016-04-07 15:28:52 -0400

    add github repo support for template

--- tests/test_readers.py
+++ tests/test_readers.py
@@ -13,9 +13,11 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+import mock
 import os
+import pytest
 
-from fossil import readers, virtual_dirs, virtual_files
+from fossil import readers, virtual_dirs
 
 
 class TestFileSystemReader(object):
@@ -40,16 +42,48 @@ class TestFileSystemReader(object):
     def test_get_template(self):
         template = self.reader.get_template()
         assert template.__class__ == virtual_dirs.TemplateRoot
-        assert len(template.children) == 3
-        assert template.children[0].__class__ == virtual_dirs.TemplateDir
-        assert template.children[0].name == "test_dir"
-        assert len(template.children[0].children) == 1
-        child = template.children[0].children[0]
-        assert child.__class__ == virtual_files.TemplateFile
-        assert child.name == "test_file"
-        assert child.content == "test\n"
-        assert template.children[1].__class__ == virtual_dirs.TemplateDir
-        assert template.children[1].name == "test_dir2"
-        assert template.children[2].__class__ == virtual_files.TemplateFile
-        assert template.children[2].name == "test_file2"
-        assert template.children[2].content == "test2\n"
+        assert len(template.children) == 2
+        names = [item.name for item in template.children]
+        assert "test_dir" in names
+        assert "test_file2" in names
+
+        if template.children[0].name == "test_dir":
+            test_dir = template.children[0]
+            test_file = template.children[1]
+        else:
+            test_file = template.children[0]
+            test_dir = template.children[1]
+        assert len(test_dir.children) == 1
+        assert test_file.content == "test2\n"
+
+        self.reader.skip = ["test_dir", "test_file2"]
+        template = self.reader.get_template()
+        names = [item.name for item in template.children]
+        assert "test_dir" not in names
+        assert "test_file2" not in names
+
+
+class TestGitRepoReader(object):
+    reader = readers.GitRepoReader("git://test/hello.git")
+
+    def test_init(self):
+        assert self.reader.path == os.path.expanduser(
+            os.path.join(
+                "~",
+                ".fossil",
+                "hello")
+        )
+        assert '.git' in self.reader.skip
+        assert self.reader.repo_url == "git://test/hello.git"
+        assert self.reader.repo_name == "hello"
+
+    def test_get_template(self):
+        with pytest.raises(Exception):
+            with mock.patch('subprocess.check_call', lambda cmd, cwd: 1):
+                self.reader.get_template()
+
+        with mock.patch('os.path.isdir', lambda x: True):
+            with mock.patch('fossil.readers.FileSystemReader.get_template',
+                            lambda x: True):
+                result = self.reader.get_template()
+                assert result
